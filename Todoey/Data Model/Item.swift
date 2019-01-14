8abc95f348108b1c0d42057a03fa2107c978e42f
Sarah <sarahmehl@gmx.de>
2019-01-14 21:08:44 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Sarah Mehl on 13.01.19.
+//  Copyright © 2019 Sarah Mehl. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
